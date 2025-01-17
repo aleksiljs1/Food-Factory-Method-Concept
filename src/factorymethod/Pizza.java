c5148c55a7cdf146724a338e656daa15bdac0d44
aleksiljs1 <aleksanderiljas@gmail.com>
2025-01-17 11:27:13 +0100

    modification_1

--- src/factorymethod/Pizza.java
+++ src/factorymethod/Pizza.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package factorymethod;
 
-/**
- *
- * @author User
- */
+
 public class Pizza extends Food{
      public void  order(){
     
